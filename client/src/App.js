ce5352c6ed32e7052d411bad6cda700cb5f285eb
Yousof Wakili <ywakili18@gmail.com>
2021-08-30 16:34:27 -0700

    cfixed directory

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import LoginPage from './LoginPage'
+
+function App() {
+  return (
+    <div>
+      <LoginPage />
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/LoginPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <form>
+      <label>Username:</label>
+      <input type="text" placeholder="Username"></input>
+      <br />
+      <br />
+      <label>Password: </label>
+      <input type="password" placeholder="Password"></input>
+    </form>
+  )
+}
+export default LoginPage
