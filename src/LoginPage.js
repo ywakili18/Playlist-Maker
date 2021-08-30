bd6f1f50940cfd24fec75591c5b9575f5b34a866
Yousof Wakili <ywakili18@gmail.com>
2021-08-30 13:37:03 -0700

    added login page with username and password

---
+++ src/LoginPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <form>
+      <label>Username</label>
+      <input type="text" placeholder="Username"></input>
+      <label>Password</label>
+      <input type="text" placeholder="Password"></input>
+    </form>
+  )
+}
+export default LoginPage
