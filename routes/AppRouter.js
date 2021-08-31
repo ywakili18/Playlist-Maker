bfd1488b682cefcf26d70dd98d493d0722ccaa3f
Yousof Wakili <ywakili18@gmail.com>
2021-08-30 19:14:46 -0700

    Fixed error with backend, fully connected React server and Backend server

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -1,8 +1,4 @@
 const { Router } = require('express')
-const ExampleRouter = require('./ExampleRouter')
-
 const AppRouter = Router()
-
-AppRouter.use('/examples', ExampleRouter)
 
 module.exports = AppRouter
