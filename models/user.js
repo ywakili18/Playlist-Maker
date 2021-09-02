9d18e2bfe335c9f3040a3edb2ca9212169d1d8e5
Yousof Wakili <ywakili18@gmail.com>
2021-09-01 20:53:13 -0700

    added pages and components

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const { Schema } = require('mongoose')
 
 const User = new Schema(
   {
-    userName: { type: String, required: true },
+    userName: { type: String, required: true, unique: true, sparse: true },
     playLists: [{ type: Schema.Types.ObjectId, ref: 'playLists' }]
   },
   { timestamps: true }
