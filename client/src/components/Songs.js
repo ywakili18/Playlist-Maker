1f9ab19e7ff11dcc26da9d2400773d06d2cbfedf
Yousof Wakili <ywakili18@gmail.com>
2021-09-01 22:47:08 -0700

    little styling

--- client/src/components/Songs.js
+++ client/src/components/Songs.js
@@ -1,13 +1,15 @@
 import React from 'react'
 const Songs = (props) => {
   return (
-    <ul>
-      {props.songs.map((song, index) => (
-        <li key={index}>
-          {song} <button onClick={() => props.removeSong(index)}>-</button>
-        </li>
-      ))}
-    </ul>
+    <div>
+      <ul>
+        {props.songs.map((song, index) => (
+          <li key={index} id="li">
+            {song} <button onClick={() => props.removeSong(index)}>-</button>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 export default Songs
