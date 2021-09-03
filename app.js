e6f92c9876f926f00c4b313b811f6941f758e5de
Yousof Wakili <ywakili18@gmail.com>
2021-09-03 08:28:23 -0700

    First heroku app

--- app.js
+++ app.js
@@ -18,9 +18,9 @@ app.use(logger('dev'))
 app.use('/api', AppRouter)
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(_dirname, 'client/build')))
+  app.use(express.static(path.join(__dirname, 'client/build')))
   app.get('*', (req, res) => {
-    res.sendFile(path.join(`${_dirname}/client/build/index.html`))
+    res.sendFile(path.join(`${__dirname}/client/build/index.html`))
   })
 }
 
