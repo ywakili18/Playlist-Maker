c5ba73ccae970a91e10565f1129144351f780f8c
Yousof Wakili <ywakili18@gmail.com>
2021-09-03 01:22:22 -0700

    Centered social tags in div

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,22 +1,21 @@
 import React from 'react'
 import { IconContext } from 'react-icons'
-import { FaGithub, FaLinkedin } from 'react-icons/fa'
+import { FaGithub, FaGoogle, FaLinkedin } from 'react-icons/fa'
 const NavBar = () => {
   return (
     <IconContext.Provider value={{ color: '#dbf9f0', size: '2em' }}>
-      <div>
-        <ul className="socialMediaIconsContainer">
-          <li>
-            <a href="https://www.linkedin.com/in/yw1818/">
-              <FaLinkedin />
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/ywakili18/Playlist-Maker">
-              <FaGithub />
-            </a>
-          </li>
-        </ul>
+      <div id="socialMediaIconsContainer">
+        <a href="https://www.linkedin.com/in/yw1818/">
+          <FaLinkedin />
+        </a>
+
+        <a href="https://github.com/ywakili18/Playlist-Maker">
+          <FaGithub />
+        </a>
+
+        <a href="https://google.com">
+          <FaGoogle />
+        </a>
       </div>
     </IconContext.Provider>
   )
